25b6c36bed64052288d213c50dec2203e99d2489
Malcolm Easton <eastonmalcolm@gmail.com>
2021-06-07 23:40:14 -0600

    copied index from previous

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const inq = require('inquirer');
 const mysql = require('mysql2');
 
 require('dotenv').config();
+
+const {buildTeam} = require('./handlers/inputHandler');
+const {saveHTML} = require('./handlers/outputHandler');
+const {buildCards, buildPage} = require('./handlers/templateHandler');
+
+buildTeam()
+    .then(buildCards)
+    .then(buildPage)
+    .then(saveHTML);
