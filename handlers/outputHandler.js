a970929f1c5bec8d03042c5c8b119a732940633f
Malcolm Easton <eastonmalcolm@gmail.com>
2021-06-12 10:58:31 -0600

    moving to new workstation

--- handlers/outputHandler.js
+++ handlers/outputHandler.js
@@ -1,9 +1,3 @@
-const fs = require('fs').promises;
-const outPath = './out/myTeam.html';
-
 module.exports = {
-    async saveHTML(htmlPage){
-        await fs.writeFile(outPath,htmlPage);
-        console.log('Wrote file to ' + outPath);
-    }
+
 }
--- index.js
+++ index.js
@@ -5,9 +5,7 @@ require('dotenv').config();
 
 const {buildTeam} = require('./handlers/inputHandler');
 const {saveHTML} = require('./handlers/outputHandler');
-const {buildCards, buildPage} = require('./handlers/templateHandler');
+const {buildCards} = require('./handlers/templateHandler');
 
 buildTeam()
     .then(buildCards)
-    .then(buildPage)
-    .then(saveHTML);
