a06283ff6851aa08fc445cb1461426476e72fcde
MEaston12 <eastonmalcolm@gmail.com>
2021-07-23 22:30:43 -0600

    set up initialization of db

--- handlers/sql.js
+++ handlers/sql.js
@@ -1,5 +1,23 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+const fs = require('fs').promises;
 
-module.exports = {
-
+class DB { //Classes are more fun
+    constructor(){
+        this.connection = {};
+    }
+    async init(){
+        this.connection = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            database: 'uucb_c12',
+            password: ''
+        });
+        console.log('Connected to database!');
+        const schema = await fs.readFile('./queries/schema.sql', 'utf-8');
+        await mysql.execute(schema);
+        console.log('Schema loaded!');
+    }
 }
+
+const db = new DB();
+module.exports = db;
