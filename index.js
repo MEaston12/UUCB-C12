b88b837009fe616fc7b92682babcac8571809d13
MEaston12 <eastonmalcolm@gmail.com>
2021-07-23 20:05:44 -0600

    cleaned up index

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const inq = require('inquirer');
-const mysql = require('mysql2');
-
 require('dotenv').config();
 
-const {buildTeam} = require('./handlers/inputHandler');
-const {saveHTML} = require('./handlers/outputHandler');
+const {buildTeam} = require('./handlers/input');
 const {buildCards} = require('./handlers/templateHandler');
 
 buildTeam()
