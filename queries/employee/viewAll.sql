3ef99e36f03f031454d019fdb1b06c46032bd8d8
MEaston12 <eastonmalcolm@gmail.com>
2021-08-01 23:40:31 -0600

    added formatting + matching for viewall

--- queries/employee/viewAll.sql
+++ queries/employee/viewAll.sql
@@ -1,1 +1,12 @@
-SELECT * FROM Employee
+SELECT 
+	Employee.id, 
+    Employee.first_name, 
+    Employee.last_name, 
+    Role.title as role, 
+    Role.salary, 
+    Department.name as department,
+    CONCAT(Manager.first_name, " ", Manager.last_name) AS manager 
+FROM Employee
+	LEFT JOIN Role ON Employee.role_id=Role.id
+    LEFT JOIN Employee Manager ON Employee.manager_id=Manager.id
+    LEFT JOIN Department ON Role.department_id=Department.id
--- queries/role/viewAll.sql
+++ queries/role/viewAll.sql
@@ -1,1 +1,2 @@
-SELECT * FROM Role
+SELECT Role.id, Role.title, Role.salary, Department.name AS department FROM Role
+	LEFT JOIN Department ON Role.department_id = Department.id;
